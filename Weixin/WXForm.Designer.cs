01160b3de973aed896d33af214521a630bdeef71
ummterry <ummterry@gmail.com>
2013-04-23 21:40:12 +0900

    Add Windows basically functioning version

---
+++ Weixin/WXForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WX
+{
+    partial class WXForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WXForm));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(984, 602);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("http://wx.qq.com", System.UriKind.Absolute);
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 400;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "微信";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // WXForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 602);
+            this.Controls.Add(this.webBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WXForm";
+            this.Text = "微信PC版";
+            this.Activated += new System.EventHandler(this.WXForm_Activated);
+            this.Deactivate += new System.EventHandler(this.WXForm_Deactivate);
+            this.Resize += new System.EventHandler(this.WXForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Weixin/WXForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace WX
+{
+    public partial class WXForm : Form
+    {
+        public WXForm()
+        {
+            InitializeComponent();
+            try
+            {
+                trayIcon = Properties.Resources.tray;
+                blankIcon = Properties.Resources.blank;
+            }
+            catch { }
+        }
+
+        string lastContactList = null, lastMsgList = null;
+        bool hasNewMsg = false, isUnfocused = false, hasNotification = false, icoFlag = false;
+        Icon trayIcon, blankIcon;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (hasNotification && trayIcon != null && blankIcon != null)
+            {
+                this.notifyIcon.Icon = (icoFlag = !icoFlag) ? trayIcon : blankIcon;
+            }
+            hasNewMsg = false;
+            HtmlElement contactList = webBrowser.Document.GetElementById("chat_conversationListContent");
+            HtmlElement msgList = webBrowser.Document.GetElementById("chat_chatmsglist");
+
+            if (contactList != null && !String.IsNullOrEmpty(contactList.InnerHtml)
+                && !String.Equals(lastContactList, contactList.InnerHtml))
+            {
+                if (lastContactList != null) hasNewMsg = true;
+                lastContactList = contactList.InnerHtml;
+            }
+
+            if (msgList != null && !String.IsNullOrEmpty(msgList.InnerHtml)
+                && !String.Equals(lastMsgList, msgList.InnerHtml))
+            {
+                if (lastMsgList != null) hasNewMsg = true;
+                lastMsgList = msgList.InnerHtml;
+            }
+
+            if (!hasNotification) hasNotification = hasNewMsg & isUnfocused;
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            this.timer.Start();
+        }
+
+        private void WXForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+
+        private void WXForm_Activated(object sender, EventArgs e)
+        {
+            this.isUnfocused = false;
+            hasNotification = false;
+            if(trayIcon != null) this.notifyIcon.Icon = trayIcon;
+        }
+
+        private void WXForm_Deactivate(object sender, EventArgs e)
+        {
+            this.isUnfocused = true;
+        }
+    }
+}
